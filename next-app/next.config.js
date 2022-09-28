144c02aa17ad7cbf07e5444f0bd387241458d19e
Eddy Vinck <eddy.vinck@isaac.nl>
2022-09-28 13:30:31 +0200

    add tailwind

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  output: 'standalone',
-}
+  output: "standalone",
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
